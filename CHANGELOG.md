24cabfc340ec496f39911cd0f9956f27137344ff
Eric Windmill <eric@ericwindmill.com>
2026-06-25 11:10:10 -0700

    initial

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 25 June, 2026
+
+Initial commit.
+
+Adds ai primitve classes, and implementations of basic-agent and minisweagent. 
---
+++ packages/dart-minisweagent/example/dart_minisweagent_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_minisweagent/dart_minisweagent.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
